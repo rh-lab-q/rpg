35fe969064e944f6b875c2abb3807a0691b9f5df
Jan Silhan <jsilhan@redhat.com>
2014-07-21 18:23:34 +0200

    ProjectBuilder.build: can return also error

--- src/project_builder.py
+++ src/project_builder.py
@@ -1,6 +1,6 @@
 class ProjectBuilder:
     def build(self, project_source_dir, project_target_dir, build_params):
-        """builds project in given project_target_dir then cleans this directory,
-        build_params is list of command strings
-           returns list of files that should be installed"""
+        """Builds project in given project_target_dir then cleans this
+           directory, build_params is list of command strings.
+           returns list of files that should be installed or error string"""
         pass
