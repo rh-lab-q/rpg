5253814e429c6d6f36b0e1ef3e8ed19100f74c78
Jan Silhan <jsilhan@redhat.com>
2014-07-21 18:24:04 +0200

    Rpg: extended API

--- src/rpg.py
+++ src/rpg.py
@@ -1,13 +1,54 @@
-class RPG(object):
+class Rpg(object):
     """Main class that is controlled by RPM GUI"""
+
+    # TODO paths will also contain hash of tarball or filename
+    self.base_path = "/tmp/rpg-<hash>/%s" 
+    self.source_extraction_path = self.base_path % "source"
+    self.project_build_path = self.base_path % "projectbuild"
+    self.rpm_stuff_path = self.base_path % "rpmstuff"
 
     def __init__(self, arg):
         self.project_builder = ProjectBuilder()
         self.package_builder = PackageBuilder()
         self.plugin_engine = PluginEngine()
-        self.source_loader = SourceLoader()
+        self.source_loader = SourceLoader(source_extraction_path)
         self.copr_uploader = CoprUploader()
         self.spec = Spec()
+
+    @property
+    def project_name():
+        return self.spec.tags["name"]
+
+    @property
+    def spec_path():
+        return rpm_stuff_path + '/' + self.project_name + ".spec"
+
+    @property
+    def tarball_path():
+        return rpm_stuff_path + '/' + self.project_name + ".tar.gz"
+
+    def process_archive_or_dir(path):
+        """executed after dir/tarball/SRPM selection"""
+        self.source_loader.load_sources(path)
+
+    def run_sources_analysis():
+        """executed in background before second screen (build commands)"""
+        self.plugin_engine.execute_phase(BEFORE_PROJECT_BUILD)
+
+    def build_project():
+        """executed in background after patch selection and reordering"""
+        self.plugin_engine.execute_phase(AFTER_PROJECT_BUILD)
+
+    def run_installed_files_analysis():
+        """executed in background after successful project build"""
+        self.plugin_engine.execute_phase(AFTER_PROJECT_BUILD)
+
+    def build_packages(*distros):
+        """builds packages for desired distributions"""
+        for distro in distros:
+            self.package_builder.build(self.spec_path, self.tarball_path,
+                                       self.distro)
+
 
     class Predictor:
         """Predictor class is used for autocompletion of field,
