0bff37ae44e7e817ca7105b783019b3399d5d53e
Jan Silhan <jsilhan@redhat.com>
2014-08-05 16:57:17 +0200

    added more plugin declaration files

--- rpg/plugin.py
+++ rpg/plugin.py
@@ -1,4 +1,15 @@
 class Plugin:
 
     """class from which are plugins derived"""
-    pass
+
+    def before_patches_aplied(self, project_dir, spec, sack):
+        # :api
+        pass
+
+    def after_patches_applied(self, project_dir, spec, sack):
+        # :api
+        pass
+
+    def after_project_build(self, project_dir, spec, sack):
+        # :api
+        pass
---
+++ rpg/plugins/misc/find_file.py
@@ -0,0 +1,7 @@
+from rpg import Plugin
+
+
+class FindFilePlugin(Plugin):
+
+    def after_project_build(self, project_dir, spec, sack):
+        pass
