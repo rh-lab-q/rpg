01daeeb42e45aef4ea8ea698046329aa052e61e5
Albert Uchytil <albert.uchytil@seznam.cz>
2014-08-29 10:59:50 +0200

    Implemented logging setup
    Fixes #19

--- RPG.py
+++ RPG.py
@@ -1,10 +1,15 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication
 from rpg.gui.wizard import Wizard
+import logging
+
 
 def main():
     import sys
- 
+
+    logging.basicConfig(filename='rpg.log', level=logging.INFO)
+    logging.info('App started')
+
     app = QApplication(sys.argv)
     wiz = Wizard()
     wiz.setObjectName("RPG")
@@ -13,8 +18,9 @@ def main():
     wiz.setMaximumSize(QtCore.QSize(850, 650))
     wiz.setBaseSize(QtCore.QSize(850, 650))
     wiz.show()
- 
+
+    logging.info('App exiting')
     sys.exit(app.exec_())
- 
+
 if __name__ == '__main__':
     main()
