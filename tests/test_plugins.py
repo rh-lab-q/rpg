bf28b507af1e181f3246c11aa161da5ae60a0330
Jan Silhan <jsilhan@redhat.com>
2014-08-29 17:02:28 +0200

    pep8: fixed test_plugins.py file

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -2,7 +2,9 @@ from support import PluginTestCase
 from rpg.plugins.misc.find_patch import FindPatchPlugin, _is_patch
 from unittest import mock
 
+
 class FindPatchPluginTest(PluginTestCase):
+
     def test_is_patch(self):
         patch = self.test_project_dir / "patch" / "0.patch"
         not_patch = self.test_project_dir / "c" / "sourcecode.c"
@@ -12,8 +14,9 @@ class FindPatchPluginTest(PluginTestCase):
     def test_find_patch(self):
         plugin = FindPatchPlugin()
         plugin.before_patches_applied(self.test_project_dir / "patch",
-            self.spec, self.sack)
+                                      self.spec, self.sack)
         patch_order = ['tests/project/patch/0.patch',
-            'tests/project/patch/1.patch', 'tests/project/patch/2.patch']
+                       'tests/project/patch/1.patch',
+                       'tests/project/patch/2.patch']
         self.assertEqual(self.spec.tags.__setitem__.call_args_list,
                          [mock.call('patch', patch_order)])
