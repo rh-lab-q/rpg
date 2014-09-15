76f85fb4ed063dc92f2dca130efbc66db1675a62
Albert Uchytil <albert.uchytil@seznam.cz>
2014-09-15 23:13:06 +0200

    Implemented MakePlugin (fix #54)

---
+++ rpg/plugins/project_builder/make.py
@@ -0,0 +1,16 @@
+from rpg.command import Command
+from rpg.plugin import Plugin
+import logging
+
+
+class MakePlugin(Plugin):
+
+    def patched(self, project_dir, spec, sack):
+        if (project_dir / "Makefile").is_file():
+            logging.debug('Makefile found')
+
+            cmd_build = Command("make")
+            cmd_install = Command("make install DESTDIR=$RPM_BUILD_ROOT")
+
+            spec.build = cmd_build
+            spec.install = cmd_install
