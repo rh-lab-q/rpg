2a4cc8d3938eccf77d965900962723bb8bf4b26d
Albert Uchytil <albert.uchytil@seznam.cz>
2014-09-15 18:09:24 +0200

    Implemented CMake plugin
    Fixes #14

--- rpg/plugins/project_builder/cmake.py
+++ rpg/plugins/project_builder/cmake.py
@@ -1,13 +1,17 @@
+from rpg.command import Command
 from rpg.plugin import Plugin
-from subprocess import call
-import os
+import logging
+
 
 class CMakePlugin(Plugin):
 
     def patched(self, project_dir, spec, sack):
         if (project_dir / "CMakeLists.txt").is_file():
-            compiled_dir = project_dir / "../compiled"
-            os.mkdir(str(compiled_dir))
-            os.chdir(str(compiled_dir))
-            call(["cmake", str(project_dir)])
-            spec.tags["build"] = "bla"
+            logging.debug('CMakeLists.txt found')
+            cmd = Command()
+            cmd.append_cmdlines("cmake " + str(project_dir))
+            cmd.append_cmdlines("make")
+            cmd.append_cmdlines("make install DESTDIR=$RPM_BUILD_ROOT")
+            cmd.rpm_variables.append(("RPM_BUILD_ROOT",
+                                      project_dir / "../installed"))
+            spec.scripts["%build"] = cmd
