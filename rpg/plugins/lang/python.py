1cdb04cdf9394723f185f369047cc106f9b32551
Albert Uchytil <albert.uchytil@seznam.cz>
2014-09-21 22:30:38 +0200

    Implemented basic PythonPlugin (fix #13)

---
+++ rpg/plugins/lang/python.py
@@ -0,0 +1,17 @@
+from rpg.plugin import Plugin
+from modulefinder import ModuleFinder
+
+
+class PythonPlugin(Plugin):
+
+    def patched(self, project_dir, spec, sack):
+            files = list(project_dir.glob('*.py'))
+            files.extend(list(project_dir.glob('*/*.py')))
+
+            mod = ModuleFinder()
+            for item in files:
+                mod.run_script(str(item))
+
+            for name, mod in mod.modules.items():
+                if mod.__file__:
+                    spec.Requires.append(mod.__file__)
