348c475c095f764d90d2d14adfe9406bd230da67
Jan Silhan <jsilhan@redhat.com>
2015-05-25 19:35:58 +0200

    tests: remove right dir after test_build_srpm

--- tests/test_build_srpm.py
+++ tests/test_build_srpm.py
@@ -1,30 +1,31 @@
 from support import RpgTestCase
 from rpg import Base
 from rpg.package_builder import PackageBuilder
-from rpg.command import Command
 from rpg.spec import Spec
 from pathlib import Path
+import shutil
+
 
 class FakeBase(Base):
-    
+
     def __init__(self):
         pass
-    
-    base_dir= Path(RpgTestCase.test_project_dir / "hello_project")
+
+    base_dir = Path(RpgTestCase.test_project_dir / "hello_project")
     project_name = "hello"
     spec = Spec()
     spec.Source = "hello-1.4.tar.gz"
-        
+
 
 class BuildSrpmTest(RpgTestCase):
-    
-    def test_build_srpm(self):
-        fbase = FakeBase()
-        PackageBuilder.build_srpm(fbase.spec_path, fbase.archive_path, fbase.base_dir)
-        self.assertTrue(fbase.srpm_path.exists())
-        
-    def TearDown():
-        base = Base()
-        Command("rm -r", base.base_dir).execute()
 
-            
+    def setUp(self):
+        self.fbase = FakeBase()
+
+    def test_build_srpm(self):
+        PackageBuilder.build_srpm(
+            self.fbase.spec_path, self.fbase.archive_path, self.fbase.base_dir)
+        self.assertTrue(self.fbase.srpm_path.exists())
+
+    def TearDown(self):
+        shutil.rmtree(str(self.fbase.base_dir))
