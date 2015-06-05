a3a237ec4a743215ffc689d688b6e12429beedbf
regeciovad <regeciovad@gmail.com>
2015-06-05 19:24:58 +0200

    Functional test added (#117)

---
+++ tests/test_functional.py
@@ -0,0 +1,53 @@
+from support import RpgTestCase
+from rpg import Base
+
+
+class FunctionalTest(RpgTestCase):
+
+    def test_c_project(self):
+        base = Base()
+        base.load_plugins()
+        base.process_archive_or_dir(
+            self.test_project_dir / "hello_project/hello-1.4.tar.gz")
+        base.spec.Name = "hello"
+        base.spec.Version = "1.4"
+        base.spec.Release = "1%{?dist}"
+        base.spec.License = "GPLv2"
+        base.spec.Summary = "Hello World test program"
+        base.spec.description = "desc"
+        base.spec.build = "make"
+        base.run_raw_sources_analysis()
+        base.run_patched_sources_analysis()
+        expected_requires = {
+            "/usr/include/gnu",
+            "/usr/include",
+            "/usr/include/sys",
+            "/usr/include/bits"
+        }
+        expected_build_requires = {
+            "/usr/include/gnu",
+            "/usr/include",
+            "/usr/include/sys",
+            "/usr/include/bits",
+            "make"
+        }
+        dirs = [
+            "Makefile", 
+            "hello.c",
+            "hello"
+        ]
+        base.run_installed_analysis()
+        self.assertEqual("make", str(base.spec.build))
+        self.assertEqual(expected_requires, set(base.spec.Requires))
+        self.assertEqual(expected_build_requires, set(base.spec.BuildRequires))
+        self.assertExistInDir(["Makefile","hello.c"], base.extracted_dir)
+        base.build_project()
+        self.assertExistInDir(dirs, base.compiled_dir)
+        base.run_compiled_analysis()
+        base.install_project()
+        base.run_installed_analysis()
+        self.assertEqual(
+            [("/hello", None, None)], base.spec.files)
+        base.write_spec()
+        base.build_srpm()
+        self.assertTrue(base.srpm_path.exists())
