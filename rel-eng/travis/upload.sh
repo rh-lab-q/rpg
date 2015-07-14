f45ea6a37ea1a3fb2752c6d6a04508ced0fe5baa
Pavol Vican <xvican01@stud.fit.vutbr.cz>
2015-07-14 11:06:35 +0200

    fix the COPR builds

--- rel-eng/travis/upload.sh
+++ rel-eng/travis/upload.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
         echo "Permission denied to upload srpm package!"
         exit 0
     fi
-    echo -e "Starting to upload build rpm\n"
+    echo -e "Starting to upload srpm package.\n"
     cd $HOME
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis"
@@ -18,10 +18,15 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
     #add, commit and push files
     git add "$package_name"
     git commit "$package_name" -m "$message"
-    git push -fq origin rpm
-    echo -e "Finish upload srpm package!\n"
-    cd $HOME/build
-    cd $TRAVIS_REPO_SLUG
-    python rel-eng/travis/upload.py $COPR_LOGIN $COPR_TOKEN $package_name
+    git push -fq origin srpm > /dev/null
+    if [ $? -eq 0 ]; then
+        echo -e "Successed upload srpm package!\n"
+        cd $HOME/build
+        cd $TRAVIS_REPO_SLUG
+        python rel-eng/travis/upload.py $COPR_LOGIN $COPR_TOKEN $package_name
+    else
+        echo -e "Failed upload srpm package!\n"
+        exit 1
+    fi
 fi
 exit 0
