7c073c1acfbd3c1b088f6fe50c460d2bfaa7ae0f
Miroslav Cibulka <xcibul10@stud.fit.vutbr.cz>
2015-09-02 15:06:46 +0200

    caching files from resolved packages (optimization of files_to_pkgs plugin)

--- rpg/plugins/misc/files_to_pkgs.py
+++ rpg/plugins/misc/files_to_pkgs.py
@@ -4,12 +4,21 @@ from rpg.plugin import Plugin
 
 class FilesToPkgsPlugin(Plugin):
 
+    TRANSLATED = {}
+
     def installed(self, project_dir, spec, sack):
         """ Resolves files in (Build) requires into packages """
         def _resolve(files, query):
             for _file in files:
                 try:
-                    yield query.filter(file=_file)[0].name
+                    if _file in self.TRANSLATED:
+                        yield self.TRANSLATED[_file]
+                    else:
+                        pckg = query.filter(file=_file)[0]
+                        self.TRANSLATED[_file] = pckg.name
+                        for _f in pckg.files:
+                            self.TRANSLATED[_f] = pckg.name
+                        yield pckg.name
                 except IndexError:
                     logging.log(logging.WARN,
                                 "For '{}' have not been found any package"
